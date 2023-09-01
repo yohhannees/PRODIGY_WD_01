56e088fae32ca97b41354a683e130b5121c0e377
Yohannees <yohannesgetachewerieso@gmail.com>
2023-09-02 01:33:21 +0300

    completed

--- main.js
+++ main.js
@@ -1,14 +1,23 @@
-
 const hamburger = document.querySelector(".hamburger");
 const navigation = document.querySelector(".navigation");
+const logo = document.querySelector(".logo");
 
 hamburger.addEventListener("click", () => {
   navigation.classList.toggle("show");
 });
 
-const menuItems = document.querySelectorAll(".navigation ul li a");
-menuItems.forEach((item) => {
-  item.addEventListener("click", () => {
-    navigation.classList.remove("show");
-  });
+// Add a click event listener to the logo to open the side panel
+logo.addEventListener("click", () => {
+  navigation.classList.add("show");
 });
+
+
+/* Set the width of the sidebar to 250px (show it) */
+function openNav() {
+  document.getElementById("mySidepanel").style.width = "250px";
+}
+
+/* Set the width of the sidebar to 0 (hide it) */
+function closeNav() {
+  document.getElementById("mySidepanel").style.width = "0";
+}
