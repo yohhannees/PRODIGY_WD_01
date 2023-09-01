b9426b4bc5206e5c2e50b8112237106ad559a4d2
Yohannees <yohannesgetachewerieso@gmail.com>
2023-09-02 00:54:22 +0300

    updated

--- main.js
+++ main.js
@@ -7,3 +7,18 @@ window.addEventListener("scroll", () => {
     navigation.style.backgroundColor = "transparent";
   }
 });
+
+const hamburger = document.querySelector('.hamburger');
+
+// Add a click event listener to toggle the side panel
+hamburger.addEventListener('click', () => {
+    navigation.classList.toggle('show');
+});
+
+// Close the side panel when a menu item is clicked
+const menuItems = document.querySelectorAll('.navigation ul li a');
+menuItems.forEach((item) => {
+    item.addEventListener('click', () => {
+        navigation.classList.remove('show');
+    });
+});
