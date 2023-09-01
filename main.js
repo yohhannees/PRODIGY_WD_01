18874aeb1e4e1d4abcd502ac5bf9fae5ee893a87
Yohannees <yohannesgetachewerieso@gmail.com>
2023-09-02 00:57:21 +0300

    hamburger menu added

--- main.js
+++ main.js
@@ -1,24 +1,14 @@
+
+const hamburger = document.querySelector(".hamburger");
 const navigation = document.querySelector(".navigation");
 
-window.addEventListener("scroll", () => {
-  if (window.scrollY > 50) {
-    navigation.style.backgroundColor = "#ff5733";
-  } else {
-    navigation.style.backgroundColor = "transparent";
-  }
+hamburger.addEventListener("click", () => {
+  navigation.classList.toggle("show");
 });
 
-const hamburger = document.querySelector('.hamburger');
-
-// Add a click event listener to toggle the side panel
-hamburger.addEventListener('click', () => {
-    navigation.classList.toggle('show');
-});
-
-// Close the side panel when a menu item is clicked
-const menuItems = document.querySelectorAll('.navigation ul li a');
+const menuItems = document.querySelectorAll(".navigation ul li a");
 menuItems.forEach((item) => {
-    item.addEventListener('click', () => {
-        navigation.classList.remove('show');
-    });
+  item.addEventListener("click", () => {
+    navigation.classList.remove("show");
+  });
 });
